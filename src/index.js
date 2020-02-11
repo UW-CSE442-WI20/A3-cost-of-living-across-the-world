d869060b191591f36c86753c741f3ece320d7372
My Tran <mtran@genemod.net>
2020-02-11 01:56:56 -0800

    loaded converted-usd data, displayed zoomable map, color scale,attempt tooltip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 "use strict";
-
 (function() {
     window.addEventListener("load", init);
 
   function init() {
+
   // The svg
   var svg = d3.select("svg"),
       width = +svg.attr("width"),
@@ -13,33 +13,51 @@
   var projection = d3.geoMercator()
       .center([0,20])                // GPS of location to zoom on
       .scale(99)                       // This is like the zoom
-      .translate([ width/2, height/2 ])
+      .translate([ width/2, height/2 ]);
 
   d3.queue()
     .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")  // World shape
-    .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_gpsLocSurfer.csv") // Position of circles
+    .defer(d3.csv, "https://gist.githubusercontent.com/mtgemo/407034207687ace3529864158ea46856/raw/ea084723ceefb3a5cfcc5a28b8141ad1a14efaec/usd-total-CoL.csv") // Position of circles
     .await(ready);
 
   function ready(error, dataGeo, data) {
-
     // Create a color scale
-    var allContinent = d3.map(data, function(d){return(d.homecontinent)}).keys()
-    var color = d3.scaleOrdinal()
-      .domain(allContinent)
-      .range(d3.schemePaired);
+    var valueExtent = d3.extent(data, function(d) { return +d.Total; })
+    var colorScale = d3.scaleSequential()
+            .domain(valueExtent)
+            .interpolator(d3.interpolateCool);
+    var g = svg.append("g");
+    // create a tooltip
+    var Tooltip =
+      svg.append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 1)
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
 
-    // Add a scale for bubble size
-    var valueExtent = d3.extent(data, function(d) { return +d.n; })
-    var size = d3.scaleSqrt()
-      .domain(valueExtent)  // What's in the data
-      .range([ 1, 50])  // Size in pixel
+    // Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(d) {
+      Tooltip.style("opacity", 1)
+    }
+    var mousemove = function(d) {
+      Tooltip
+        .html(d.City + ", " + d.Country + "<br>" + "Total COL: " + d.Total + "$")
+        .style("left", (d3.mouse(this)[0]+10) + "px")
+        .style("top", (d3.mouse(this)[1]) + "px")
+    }
+    var mouseleave = function(d) {
+      Tooltip.style("opacity", 0)
+    }
 
     // Draw the map
-    svg.append("g")
-        .selectAll("path")
+    g.selectAll("path")
         .data(dataGeo.features)
         .enter()
         .append("path")
+          .attr("transform", function(d) { return "translate(" + d + ")"; })
           .attr("fill", "#b8b8b8")
           .attr("d", d3.geoPath()
               .projection(projection)
@@ -48,20 +66,32 @@
         .style("opacity", .3)
 
     // Add circles:
-    svg
-      .selectAll("myCircles")
-      .data(data.sort(function(a,b) { return +b.n - +a.n }).filter(function(d,i){ return i<1000 }))
-      .enter()
-      .append("circle")
-        .attr("cx", function(d){ return projection([+d.homelon, +d.homelat])[0] })
-        .attr("cy", function(d){ return projection([+d.homelon, +d.homelat])[1] })
-        .attr("r", function(d){ return size(+d.n) })
-        .style("fill", function(d){ return color(d.homecontinent) })
-        .attr("stroke", function(d){ if(d.n>2000){return "black"}else{return "none"}  })
-        .attr("stroke-width", 1)
-        .attr("fill-opacity", .4)
+    var circle = g.selectAll("circle")
+        .data(data)
+        .enter().append("circle")
+            .attr("r", 3)
+            .attr("cx", function(d){ return projection([+d.Longitude, +d.Latitude])[0] })
+            .attr("cy", function(d){ return projection([+d.Longitude, +d.Latitude])[1] })
+            .style("fill", function(d){return colorScale(+d.Total)})
+            .attr("fill-opacity", .7)
+            .attr("stroke", "black")
+            .attr("stroke-width", 0.2)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
 
+    svg.append("rect")
+        .attr("fill", "none")
+        .attr("pointer-events", "all")
+        .attr("width", width)
+        .attr("height", height)
+        .call(d3.zoom()
+            .scaleExtent([1, 8])
+            .on("zoom", zoom));
 
+    function zoom() {
+        g.attr("transform", d3.event.transform);
+    }
 
     // Add title and explanation
     svg
@@ -71,16 +101,19 @@
         .attr("x", width - 10)
         .attr("y", height - 30)
         .attr("width", 90)
-        .html("WHERE SURFERS LIVE")
+        .html("COST OF LIVING")
         .style("font-size", 14)
 
 
     // --------------- //
     // ADD LEGEND //
     // --------------- //
-
+    legend({
+      color: d3.scaleSequential([0, 100], d3.interpolateViridis),
+      title: "Temperature (°F)"
+    })
     // Add legend: circles
-    var valuesToShow = [100,4000,15000]
+    var valuesToShow = [1000,40000,150000]
     var xCircle = 40
     var xLabel = 90
     svg
